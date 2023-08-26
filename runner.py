4a7553fa674c43b9b60b34d6222a900924119252
Igor Diachuk <eparadize@gmail.com>
2023-08-26 21:27:09 +0200

    cleaned runner.py

--- runner.py
+++ runner.py
@@ -2,5 +2,3 @@ import subprocess
 
 s = subprocess.run(f'python -m pytest -s -v --alluredir=allure_report tests/', shell=True, check=False)
 s = subprocess.run(f'allure serve allure_report --port 8080', shell=True, check=False)
-
-# python -m pytest -s -v --headed --screenshot=only-on-failure --slowmo=500 --numprocesses=2 --alluredir=allure_report tests/test_purchase_positive.py
